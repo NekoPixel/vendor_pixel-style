1027b18b5cea2cf1ae08a3de4762c3093271c111
Dyneteve <dyneteve@hentaios.com>
2024-04-27 00:43:53 +0000

    pixel-style: Initial commit
    * Audio
    * Charger
    * Common
    * RRO overlays
    
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ config/rro_overlays.mk
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2024 The hentaiOS Project and its Proprietors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Google Overlays
+PRODUCT_PACKAGES += \
+    AccessibilityMenuOverlay \
+    BuiltInPrintServiceOverlay \
+    CaptivePortalLoginOverlay \
+    CellBroadcastReceiverOverlay \
+    CellBroadcastServiceOverlay \
+    ContactsProviderOverlay \
+    DMServiceOverlay \
+    FlipendoOverlay \
+    GoogleConfigOverlay \
+    GoogleDeviceLockControllerOverlay \
+    GoogleHealthConnectOverlay \
+    GoogleWebViewOverlay \
+    MediaProviderOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlayCommon \
+    PixelDocumentsUIGoogleOverlay \
+    PixelFwOverlay \
+    PixelLauncherCustomOverlay \
+    PixelSetupWizardOverlay \
+    SettingsGoogleOverlay \
+    SettingsProviderOverlay \
+    SystemUIGoogleOverlay \
+    TeleServiceOverlay \
+    TelecomOverlay \
+    TelephonyProviderOverlay
+
+ifeq ($(TARGET_IS_PIXEL), true)
+PRODUCT_PACKAGES += \
+    ManagedProvisioningPixelOverlay \
+    PixelBatteryHealthOverlay
+endif
+
+ifeq ($(TARGET_IS_PIXEL_7), true)
+PRODUCT_PACKAGES += \
+    PixelConnectivityOverlay2022
+endif
+
+ifeq ($(TARGET_PIXEL_STAND_SUPPORTED), true)
+PRODUCT_PACKAGES += \
+    DreamlinerOverlay
+endif
